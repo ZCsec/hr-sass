996b710af7c02aac0662a7138681483d6e175fb6
Liulf <89725139@qq.com>
2023-02-21 17:13:36 +0800

    第二次提交框架

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,62 @@
 <template>
   <div>
-    Layout
-    <router-view></router-view>
+    <el-container style="height: 690px; border: 1px solid #eee">
+      <el-aside
+        width="200px"
+        style="background-color: rgb(75, 124, 251); height: 100%"
+      >
+        <el-menu text-color="#fff" style="background-color: rgb(75, 124, 251)">
+          <div class="logo"><img src="@/assets/common/logo.png" alt="" /></div>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">首页</router-link>
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home"
+              >组织架构</router-link
+            >
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">员工</router-link>
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home"
+              >公司设置</router-link
+            >
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home"
+              >权限设置</router-link
+            >
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">社保</router-link>
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">考勤</router-link>
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">工资</router-link>
+          </el-menu-item>
+          <el-menu-item>
+            <router-link class="router-l" to="/layout/home">审批</router-link>
+          </el-menu-item>
+        </el-menu>
+        <div class="left-bottom-bg"></div>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+          <span style="text-align: left; color: #fff"
+            >南昌中兴软件技术有限公司</span
+          >
+          
+        </el-header>
+
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -9,4 +64,55 @@
 export default {}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.el-header {
+  background-color: rgb(75, 124, 251);
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  position: relative !important;
+  box-shadow: 3px 0 10px 0 rgba(0, 0, 0, 0.6) !important;
+
+  .el-menu {
+    border-right: 0;
+  }
+
+  .el-menu-item:hover {
+    color: rgb(75, 124, 251) !important;
+  }
+
+  .logo {
+    margin: 10px 0 10px 20px;
+    img {
+      display: inline-block;
+    }
+  }
+
+  .left-bottom-bg {
+    // border: 1px solid red;
+    position: absolute;
+    height: 100px;
+    width: 100%;
+    background-image: url('@/assets/common/leftnavBg.png');
+    background-position: 0 -50px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    bottom: 0;
+  }
+}
+
+.router-l {
+  display: inline-block;
+  width: 100%;
+  color: #fff !important;
+  text-decoration: none;
+  font-size: 16px;
+  text-align: center;
+
+  &:hover {
+    color: rgb(75, 124, 251) !important;
+  }
+}
+</style>
